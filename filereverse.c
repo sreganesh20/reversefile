6f7269c32e7f8c9e114d714affa6dd7fc66fa6b1
sreganesh20 <65004166+sreganesh20@users.noreply.github.com>
2020-05-25 09:47:23 +0530

    reversefile.c

---
+++ filereverse.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+      FILE *fp;
+      char ch;
+      int i,pos;
+      char sentence[100];
+      fp=fopen("input.txt","w");
+      fgets(sentence, sizeof(sentence), stdin);
+      fprintf(fp, "%s", sentence);
+      fclose(fp);
+      fp=fopen("input.txt","r");
+      if(fp==NULL)
+      {
+            printf("File does not exist..");
+      }
+      fseek(fp,0,SEEK_END);
+      pos=ftell(fp);
+      //printf("Current postion is %d\n",pos);
+      i=0;
+      while(i<pos)
+       {
+            i++;
+            fseek(fp,-i,SEEK_END);
+            //printf("%c",fgetc(fp));
+            ch=fgetc(fp);
+            printf("%c",ch);
+      }
+      return 0;
+}
